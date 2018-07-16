b859efeee669da240ff7bb571a9f4a4849aab95c
wkke <30410996@qq.com>
2018-07-16 23:17:39 +0800

    解决postcss版本更新不兼容问题

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -8,57 +8,57 @@ const { resolve } = require("./bundle")
 const { externals } = require("./externals")
 const webpackBaseFn = require("./webpack.config.base");
 
-module.exports = function(env,{ option }){
-  const baseConfig = webpackBaseFn(env)
-  const reportOn = option === "report"
-  const plugins = [
-    new MiniCssExtractPlugin({
-      filename: "css/[name].[hash].css",
-    }),
-  ];
-  if(reportOn){
-    plugins.push(new BundleAnalyzerPlugin())
-  }
-
-  return webpackMerge(baseConfig,{
-    mode:"production",
-    optimization:{
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            test: /[\\/]node_modules[\\/]/,
-            name: "vendor",
-            chunks: "all"
-          }
-        }
-      },
-      minimizer: [
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            compress: {
-              warnings: false,
-              drop_debugger: false,
-              drop_console: true
-            }
-          }
+module.exports = function(env, { option }) {
+    const baseConfig = webpackBaseFn(env)
+    const reportOn = option === "report"
+    const plugins = [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[hash].css",
         }),
-        new OptimizeCSSAssetsPlugin({
-          cssProcessorOptions: {
-            safe: true
-          }
-        })
-      ]
-    },
-    stats:{
-      chunkGroups: false,
-      chunkModules: false,
-      chunkOrigins: false,
-      modules: false,
-      moduleTrace: false,
-      source: false,
-      children: false,
-    },
-    externals,
-    plugins
-  });
+    ];
+    if (reportOn) {
+        plugins.push(new BundleAnalyzerPlugin())
+    }
+
+    return webpackMerge(baseConfig, {
+        mode: "production",
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    commons: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "vendor",
+                        chunks: "all"
+                    }
+                }
+            },
+            minimizer: [
+                new UglifyJsPlugin({
+                    uglifyOptions: {
+                        compress: {
+                            warnings: false,
+                            drop_debugger: false,
+                            drop_console: true
+                        }
+                    }
+                }),
+                new OptimizeCSSAssetsPlugin({
+                    cssProcessorOptions: {
+                        //safe: true //解决postcss版本更新不兼容问题
+                    }
+                })
+            ]
+        },
+        stats: {
+            chunkGroups: false,
+            chunkModules: false,
+            chunkOrigins: false,
+            modules: false,
+            moduleTrace: false,
+            source: false,
+            children: false,
+        },
+        externals,
+        plugins
+    });
 }
